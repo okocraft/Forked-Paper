6f11d15fe2b34183929d3eb65568d553cecdf2cd
Siroshun09 <siro.shun8@gmail.com>
2022-09-02 23:28:47 +0900

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+    id("io.papermc.paperweight.patcher") version "1.3.8"
+}
+
+val paperMavenPublicUrl = "https://papermc.io/repo/repository/maven-public/"
+
+repositories {
+    mavenCentral()
+    maven(paperMavenPublicUrl) {
+        content { onlyForConfigurations(configurations.paperclip.name) }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
+    decompiler("net.minecraftforge:forgeflower:1.5.605.7")
+    paperclip("io.papermc:paperclip:3.0.2")
+}
+
+allprojects {
+    apply(plugin = "java")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+}
+
+subprojects {
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    repositories {
+        mavenCentral()
+        maven(paperMavenPublicUrl)
+    }
+}
+
+paperweight {
+    serverProject.set(project(":okocraft-server"))
+
+    remapRepo.set(paperMavenPublicUrl)
+    decompileRepo.set(paperMavenPublicUrl)
+
+    usePaperUpstream(providers.gradleProperty("paperRef")) {
+        withPaperPatcher {
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            apiOutputDir.set(layout.projectDirectory.dir("okocraft-api"))
+
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+            serverOutputDir.set(layout.projectDirectory.dir("okocraft-server"))
+        }
+    }
+}
+
