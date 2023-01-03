e7a80df7fba52d1941db46a0d9ee86833d423919
Siroshun09 <siro.shun8@gmail.com>
2023-01-03 15:36:24 +0900

    Remove ci skip from commit messages

--- scripts/check-upstream.sh
+++ scripts/check-upstream.sh
@@ -10,7 +10,7 @@ if [ $CURRENT_HASH = $LATEST_COMMIT_HASH ]; then
   exit 0
 fi
 
-COMMITS=`git log --reverse --pretty=format:"%h %s" $CURRENT_HASH..$LATEST_COMMIT_HASH | sed -e 's/\(#[0-9]*\)/Paper\1/g'`
+COMMITS=`git log --reverse --pretty=format:"%h %s" $CURRENT_HASH..$LATEST_COMMIT_HASH | sed -e 's/\(#[0-9]*\)/Paper\1/g' | sed -e 's/\[ci skip\] //'`
 
 echo "Paper Changes:"
 echo "$COMMITS"
