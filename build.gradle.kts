37901397be7976527ef773f7dd14568ed48a091d
Siroshun09 <siro.shun8@gmail.com>
2023-03-15 07:53:49 +0900

    work [ci skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ subprojects {
 }
 
 paperweight {
-    serverProject.set(project(":pufferfish-server"))
+    serverProject.set(project(":okocraft-server"))
 
     remapRepo.set("https://maven.fabricmc.net/")
     decompileRepo.set("https://files.minecraftforge.net/maven/")
@@ -55,8 +55,8 @@ paperweight {
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
 
-            apiOutputDir.set(layout.projectDirectory.dir("pufferfish-api"))
-            serverOutputDir.set(layout.projectDirectory.dir("pufferfish-server"))
+            apiOutputDir.set(layout.projectDirectory.dir("okocraft-api"))
+            serverOutputDir.set(layout.projectDirectory.dir("okocraft-server"))
         }
     }
 }
