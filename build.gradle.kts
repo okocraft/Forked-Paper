b3e112e77c15a5986c542064f85a467c3adf84c3
Siroshun09 <siro.shun8@gmail.com>
2023-04-16 08:01:09 +0900

    build: update shadow to 8.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
 
 plugins {
     java
-    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
     id("io.papermc.paperweight.patcher") version "1.5.4"
 }
 
