3a4dc7dca20dbfb63abfc80ee30736771d694e89
Siroshun09 <siro.shun8@gmail.com>
2024-03-25 15:42:12 +0900

    build: update tiny-remapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    remapper("net.fabricmc:tiny-remapper:0.10.1:fat")
     decompiler("net.minecraftforge:forgeflower:2.0.629.2")
     paperclip("io.papermc:paperclip:3.0.3")
 }
