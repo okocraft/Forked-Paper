6f368b0365b4ebd138cd7e2e143345dcb8f0d3df
Siroshun09 <39247022+Siroshun09@users.noreply.github.com>
2026-08-11 05:35:21 +0000

    Updated Upstream (Folia) Folia Changes: dfe4091 Update Paper 24c5c95 Update Paper
    Latest commit hash: 24c5c95dc45e02caff98a97ed6ffee7565523464
    See full diff at https://github.com/PaperMC/Folia/compare/57f643f10e0a9d01024773232d38ae666067d593...24c5c95dc45e02caff98a97ed6ffee7565523464

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,11 @@ paperweight {
             outputFile = file("okocraft-api/build.gradle.kts")
             patchFile = file("okocraft-api/build.gradle.kts.patch")
         }
+        patchFile {
+            path = "folia-checkstyle/build.gradle.kts"
+            outputFile = file("okocraft-checkstyle/build.gradle.kts")
+            patchFile = file("okocraft-checkstyle/build.gradle.kts.patch")
+        }
         patchRepo("paperApi") {
             upstreamPath = "paper-api"
             excludes = setOf("build.gradle.kts")
@@ -31,6 +36,23 @@ paperweight {
             excludes = listOf("build.gradle.kts", "build.gradle.kts.patch", "paper-patches")
             patchesDir = file("okocraft-api/folia-patches")
             outputDir = file("folia-api")
+        }
+        patchRepo("paperCheckstyle") {
+            upstreamPath = "paper-checkstyle"
+            excludes = setOf("build.gradle.kts")
+            patchesDir = file("okocraft-checkstyle/paper-patches")
+            outputDir = file("paper-checkstyle")
+        }
+        patchDir("foliaCheckstyle") {
+            upstreamPath = "folia-checkstyle"
+            excludes = setOf("build.gradle.kts")
+            patchesDir = file("okocraft-checkstyle/folia-patches")
+            outputDir = file("folia-checkstyle")
+        }
+        patchRepo("paperCheckstyleConfig") {
+            upstreamPath = ".checkstyle"
+            patchesDir = file("okocraft-checkstyle/config-patches")
+            outputDir = file(".checkstyle")
         }
     }
 }
